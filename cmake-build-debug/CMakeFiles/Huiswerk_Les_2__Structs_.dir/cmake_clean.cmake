9352031f12e2c7d7d654886185c255521423e9db
Rens-Surface <rvanvendeloo@gressive.nl>
2021-09-24 13:34:58 +0200

    Init of Repository

---
+++ cmake-build-debug/CMakeFiles/Huiswerk_Les_2__Structs_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Huiswerk_Les_2__Structs_.dir/main.cpp.obj"
+  "Huiswerk_Les_2__Structs_.exe"
+  "Huiswerk_Les_2__Structs_.exe.manifest"
+  "Huiswerk_Les_2__Structs_.pdb"
+  "libHuiswerk_Les_2__Structs_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Huiswerk_Les_2__Structs_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
