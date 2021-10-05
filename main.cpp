0477c93050fc96061c8e1c1ffb091fa1b00d3e63
Rens-Surface <rvanvendeloo@gressive.nl>
2021-10-05 19:04:30 +0200

    Init of Repository

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+struct Vec3D {float x, y, z;};
+
+Vec3D vec3D (float x, float y, float z){
+    Vec3D self;
+    self.x = x;
+    self.y = y;
+    self.z = z;
+    return self ;
+}
+
+void show = (std::string label, Vec3D const &self ){
+
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl << " Bye world!" << std::endl;
     return 0;
